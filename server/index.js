bec037085ff7f8da632b157974b2c7e3d01b9833
i-ahkSH <truongquangkhai151020@gmail.com>
2020-04-26 17:24:38 +0700

    1.0.0

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+const router = require('./router');
+
+const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
+
+io.on('connect', (socket) => {
+    console.log('we have a new connection!!!');
+    socket.on('disconnect', () => {
+        console.log('User has left');
+    });
+    
+});
+
+app.use(router);
+server.listen(PORT, () => {console.log(`server is running on port ${PORT}`)});
